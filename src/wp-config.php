de07dfe0f24a2b520e5c6694e770bc66ae9bc663
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-05-04 16:22:10 +0200

    app-network set

--- src/wp-config.php
+++ src/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'tiger' );
 
 /** Database hostname */
-define( 'DB_HOST', 'db' );
+define( 'DB_HOST', 'app-network' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
