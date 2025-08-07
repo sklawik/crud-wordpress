bb3ad8bc2ed560b25d4bfe28b512f222dda298d4
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-08-07 13:51:40 +0200

    .env template

--- src/wp-content/plugins/ajax-php-plugin/php/db.php
+++ src/wp-content/plugins/ajax-php-plugin/php/db.php
@@ -1,12 +1,13 @@
 <?php
 
 
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'phpdb');
 
 function getDatabaseConnection()
 {
     static $conn = null;
     if ($conn === null) {
-        $conn = mysqli_connect("phpdb", "root", "tiger", "my_database");
+        $conn = mysqli_connect(DB_HOST, "root", "tiger", "my_database");
 
         if (!$conn) {
             error_log("Błąd połączenia z bazą: " . mysqli_connect_error());
